83df6defa5d00981d807a147c64fd5cf997b1cba
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:45:57 -0500

    deletedCommits

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+    private final boolean wheels;
+    private final int cylinders;
+
+    public Car(boolean wheels, int cylinders) {
+        this.wheels = wheels;
+        this.cylinders = cylinders;
+    }
+
+    public boolean isWheels() {
+        return wheels;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+}
